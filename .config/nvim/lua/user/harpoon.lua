a67e1ca2ec1735085f915f24ce3db89d36c8fa85
Mitchell Gerber <mgerb42@gmail.com>
2022-05-05 08:27:17 -0500

    fix(nvim): update harpoon width

--- .config/nvim/lua/user/harpoon.lua
+++ .config/nvim/lua/user/harpoon.lua
@@ -18,6 +18,10 @@ harpoon.setup({
 
 	-- set marks specific to each git branch inside git repository
 	mark_branch = false,
+
+  menu = {
+    width = vim.api.nvim_win_get_width(0) - 40
+  }
 })
 
 require("telescope").load_extension("harpoon")
