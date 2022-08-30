ccd323866739e4746b2833314bb438ceef1c4873
Mitchell Gerber <mgerb42@gmail.com>
2022-08-30 07:46:07 -0500

    nvim: bufferline - comment

--- .config/nvim/lua/user/bufferline.lua
+++ .config/nvim/lua/user/bufferline.lua
@@ -15,7 +15,11 @@ bufferline.setup({
 		-- NOTE: this plugin is designed with this icon in mind,
 		-- and so changing this is NOT recommended, this is intended
 		-- as an escape hatch for people who cannot bear it for whatever reason
-		indicator_icon = "▎",
+		indicator = {
+			-- icon = "▎",
+			-- style = "icon",
+			style = "none",
+		},
 		buffer_close_icon = "",
 		modified_icon = "●",
 		close_icon = "",
@@ -38,7 +42,7 @@ bufferline.setup({
 		diagnostics_update_in_insert = false,
 		diagnostics_indicator = function(count, level, diagnostics_dict, context)
 			-- return "(" .. count .. ")"
-      local icon = level:match("error") and " " or " "
+			local icon = level:match("error") and " " or " "
 			return icon
 		end,
 		offsets = { { filetype = "NvimTree", text = "", text_align = "left" } },
@@ -51,7 +55,7 @@ bufferline.setup({
 		persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
 		-- can also be a table containing 2 custom separators
 		-- [focused and unfocused]. eg: { '|', '|' }
-		separator_style = "thick", -- | "thick" | "thin" | "slant" | { 'any', 'any' }
+		separator_style = "thin", -- | "thick" | "thin" | "slant" | { 'any', 'any' }
 		enforce_regular_tabs = true,
 		always_show_bufferline = true,
 		-- sort_by = 'insert_after_current' |'insert_at_end' | 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
