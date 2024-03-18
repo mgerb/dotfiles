5ea6967c5798bc40a858f45acb6fe793078616e5
Mitchell <mgerb42@gmail.com>
2024-03-17 22:00:46 -0500

    nvim: set terminal to powershell on windows

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,14 @@
 require("mg")
 
+-- Set the shell to Powershell on Windows
+if vim.fn.has("win32") == 1 then
+	vim.cmd("set shell=powershell.exe")
+	vim.cmd("set shellcmdflag=-NoProfile\\ -Command")
+end
+
+-- Set the current working directory to the directory of
+-- the current file or path in which Neovim was opened with.
+-- e.g. `nvim . or nvim /path/to/file`
 local function update_directory(data)
 	--buffer is a directory
 	local directory = vim.fn.isdirectory(data.file) == 1
@@ -12,7 +21,6 @@ local function update_directory(data)
 	vim.cmd.cd(data.file)
 end
 
--- change to the directory of the current file
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = update_directory })
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
