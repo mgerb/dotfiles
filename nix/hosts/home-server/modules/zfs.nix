1b0c96b28baea89317517ce961a53739a05aaf27
Mitchell <mgerb42@gmail.com>
2024-09-02 10:55:05 -0500

    home-server: add zfs auto snapshot

--- nix/hosts/home-server/modules/zfs.nix
+++ nix/hosts/home-server/modules/zfs.nix
@@ -1,7 +1,24 @@
+# lsit snapshots
+# `zfs list -t snapshot`
+#
+# Prune snapshots over a week old
+# `zfs-prune-snapshots 1w`
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     zfs
+    zfs-prune-snapshots
   ];
   boot.supportedFilesystems = ["zfs"];
   boot.zfs.extraPools = ["raid-pool" "wd1" "wd2"];
+
+  services.zfs.autoScrub.enable = true;
+
+  services.zfs.autoSnapshot = {
+    enable = true;
+    frequent = 4;
+    hourly = 24;
+    daily = 7;
+    weekly = 4;
+    monthly = 4;
+  };
 }
