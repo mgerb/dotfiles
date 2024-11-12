fd47ff3f284ebeaea39fc2ba3ddf69b663e83de6
Mitchell <mgerb42@gmail.com>
2024-11-12 17:24:46 -0600

    nix: desktop - update nvidia drivers - other misc changes

--- nix/hosts/desktop/nvidia.nix
+++ nix/hosts/desktop/nvidia.nix
@@ -19,7 +19,7 @@
     # Enable this if you have graphical corruption issues or application crashes after waking
     # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
     # of just the bare essentials.
-    powerManagement.enable = false;
+    powerManagement.enable = true;
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
@@ -39,6 +39,6 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 }
