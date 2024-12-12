149e3d2d87e144ed7f98e397ad4717636f89a715
Mitchell <mgerb42@gmail.com>
2024-12-12 00:41:41 -0600

    nvim: try new color scheme

--- config/nvim/lua/plugins/colors.lua
+++ config/nvim/lua/plugins/colors.lua
@@ -1,8 +1,19 @@
 return {
-	"rebelot/kanagawa.nvim",
 	lazy = false,
 	priority = 1000,
+	"catppuccin/nvim",
+	name = "catppuccin",
 	config = function()
-		vim.cmd("colorscheme kanagawa-wave")
+		vim.cmd.colorscheme("catppuccin-mocha")
 	end,
 }
+
+-- kanagawa
+-- return {
+-- 	"rebelot/kanagawa.nvim",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	config = function()
+-- 		vim.cmd("colorscheme kanagawa-wave")
+-- 	end,
+-- }
