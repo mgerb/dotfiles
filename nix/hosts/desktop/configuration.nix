4c65c5a8f02dd99458cc34d4b4aea5d5f6bdb509
Mitchell <mgerb42@gmail.com>
2025-01-05 11:33:11 -0600

    nix: update desktop, add ghostty

--- nix/hosts/desktop/configuration.nix
+++ nix/hosts/desktop/configuration.nix
@@ -110,6 +110,7 @@
   environment.systemPackages = with pkgs; [
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
+    ghostty
   ];
 
   # for steam
