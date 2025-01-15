2e6a7e3327f03d864693f508253f86c3c0fd71f3
Mitchell <mgerb42@gmail.com>
2025-01-14 23:00:05 -0600

    nvim: fix telescope on windows

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -8,11 +8,13 @@ return {
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 	},
 	config = function()
-		require("telescope").setup({
-			extensions = {
-				fzf = {},
-			},
-		})
-		require("telescope").load_extension("fzf")
+		if vim.fn.has("win32") ~= 1 then
+			require("telescope").setup({
+				extensions = {
+					fzf = {},
+				},
+			})
+			require("telescope").load_extension("fzf")
+		end
 	end,
 }
