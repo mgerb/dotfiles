fe41447e9ea43b9b549cedbb0d32d86fb6be4857
Mitchell <mgerb42@gmail.com>
2025-01-22 09:07:57 -0600

    nvim: use git bash on windows

--- config/nvim/lua/mg/_windows.lua
+++ config/nvim/lua/mg/_windows.lua
@@ -2,6 +2,10 @@
 
 -- Set the shell to Powershell on Windows
 if vim.fn.has("win32") == 1 then
-	vim.cmd("set shell=powershell.exe")
+	if vim.fn.executable("bash.exe") then
+		vim.cmd("set shell=bash.exe")
+	else
+		vim.cmd("set shell=powershell.exe")
+	end
 	vim.cmd("set shellcmdflag=-NoProfile\\ -Command")
 end
