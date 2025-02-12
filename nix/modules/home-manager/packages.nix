39955fffd1b99b0b61ef0e2b4be626e3d3cc1137
Mitchell <mgerb42@gmail.com>
2025-02-12 00:51:01 -0600

    nix: add overlays for zig/zls master versions

--- nix/modules/home-manager/packages.nix
+++ nix/modules/home-manager/packages.nix
@@ -1,4 +1,35 @@
 {pkgs, ...}: {
+  nixpkgs.overlays = [
+    (final: prev: {
+      zig-custom = prev.stdenv.mkDerivation {
+        pname = "zig";
+        version = "0.14.0-dev.3197";
+        src = prev.fetchurl {
+          url = "https://ziglang.org/builds/zig-linux-x86_64-0.14.0-dev.3197+1d8857bbe.tar.xz";
+          sha256 = "sha256-o72foPkVmulUVc2iUpynV1wzI/+2h/So1c48lZQXIxQ=";
+        };
+        sourceRoot = ".";
+        installPhase = ''
+          mkdir -p $out/bin
+          mv zig-linux-x86_64-*/* $out/bin/
+        '';
+      };
+      zls-custom = prev.stdenv.mkDerivation {
+        pname = "zls";
+        version = "0.14.0-dev.383";
+        src = prev.fetchurl {
+          url = "https://builds.zigtools.org/zls-linux-x86_64-0.14.0-dev.383+f06cee3.tar.xz";
+          sha256 = "sha256-fj2xIR5GWVQlJiEwJVrFeXMyyctuYOXr8N5Q33nHORc=";
+        };
+        sourceRoot = ".";
+        installPhase = ''
+          mkdir -p $out/bin
+          mv zls $out/bin/
+        '';
+      };
+    })
+  ];
+
   home.packages = with pkgs; [
     vim
     killall
@@ -31,8 +62,8 @@
     poetry
     pyright
 
-    zig
-    zls
+    zig-custom
+    zls-custom
 
     # neovim
     neovim
