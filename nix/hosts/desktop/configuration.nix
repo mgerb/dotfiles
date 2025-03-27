924f7aa7f7caa46230e6d6856346d055cb540043
Mitchell <mgerb42@gmail.com>
2025-03-26 23:00:58 -0500

    nix: update desktop

--- nix/hosts/desktop/configuration.nix
+++ nix/hosts/desktop/configuration.nix
@@ -110,6 +110,7 @@
   environment.systemPackages = with pkgs; [
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
+    obs-studio
   ];
 
   # for steam
