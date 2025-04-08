947ba0f984189956dc1ba6c2a1db7acaebbe48ae
Mitchell <mgerb42@gmail.com>
2025-04-08 00:40:43 -0500

    nvim: update colors

--- config/nvim/lua/plugins/colors.lua
+++ config/nvim/lua/plugins/colors.lua
@@ -1,19 +1,19 @@
 return {
-	lazy = false,
 	priority = 1000,
 	"catppuccin/nvim",
 	name = "catppuccin",
 	config = function()
+		local color = "#00000f"
+		require("catppuccin").setup({
+			flavour = "mocha",
+			color_overrides = {
+				mocha = {
+					base = color,
+					mantle = color,
+					crust = color,
+				},
+			},
+		})
 		vim.cmd.colorscheme("catppuccin-mocha")
 	end,
 }
-
--- kanagawa
--- return {
--- 	"rebelot/kanagawa.nvim",
--- 	lazy = false,
--- 	priority = 1000,
--- 	config = function()
--- 		vim.cmd("colorscheme kanagawa-wave")
--- 	end,
--- }
