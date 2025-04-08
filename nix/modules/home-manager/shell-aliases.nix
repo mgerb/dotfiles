076b9bb5fffc2c03a302847a5b1f93bda2859835
Mitchell <mgerb42@gmail.com>
2025-04-07 22:10:36 -0500

    nix: update desktop, shell aliases

--- nix/modules/home-manager/shell-aliases.nix
+++ nix/modules/home-manager/shell-aliases.nix
@@ -1,8 +1,9 @@
 {...}: {
   home.shellAliases = {
+    hm = "home-manager";
     dc = "docker compose";
     # docker compose pull, down, update
     mgdu = "dc pull && dc down && dc up -d";
-    hm = "home-manager";
+    mgspace = "du -h -d 1 | sort -h";
   };
 }
