e0141405e66ab570a4e39179aeba3ce162387a17
Mitchell <mgerb42@gmail.com>
2025-04-09 00:40:11 -0500

    nvim: update colors again

--- config/nvim/lua/plugins/colors.lua
+++ config/nvim/lua/plugins/colors.lua
@@ -3,14 +3,28 @@ return {
 	"catppuccin/nvim",
 	name = "catppuccin",
 	config = function()
-		local color = "#00000f"
+		-- local color = "#00000f"
 		require("catppuccin").setup({
 			flavour = "mocha",
 			color_overrides = {
+				-- mocha = {
+				-- 	base = color,
+				-- 	mantle = color,
+				-- 	crust = color,
+				-- },
+
+				-- defaults
+				-- mocha = {
+				-- 	base = "#1e1e2e",
+				-- 	mantle = "#181825",
+				-- 	crust = "#11111b",
+				-- },
+
+				-- darken defaults by 10%
 				mocha = {
-					base = color,
-					mantle = color,
-					crust = color,
+					base = "#0f0f17",
+					mantle = "#0c0c12",
+					crust = "#08080d",
 				},
 			},
 		})
