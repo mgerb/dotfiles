426122086c46fa81bba778bd6d5a2c76851c4131
Mitchell <mgerb42@gmail.com>
2025-05-18 22:46:26 -0500

    nvim: just use default windows terminal

--- config/nvim/lua/mg/_windows.lua
+++ config/nvim/lua/mg/_windows.lua
@@ -1,11 +1,11 @@
 --- Window specific options
 
 -- Set the shell to Powershell on Windows
-if vim.fn.has("win32") == 1 then
-	if vim.fn.executable("bash.exe") then
-		vim.cmd("set shell=bash.exe")
-	else
-		vim.cmd("set shell=powershell.exe")
-	end
-	vim.cmd("set shellcmdflag=-NoProfile\\ -Command")
-end
+-- if vim.fn.has("win32") == 1 then
+-- 	if vim.fn.executable("bash.exe") then
+-- 		vim.cmd("set shell=bash.exe")
+-- 	else
+-- 		vim.cmd("set shell=powershell.exe")
+-- 	end
+-- 	vim.cmd("set shellcmdflag=-NoProfile\\ -Command")
+-- end
