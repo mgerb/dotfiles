49a1608659042c16a9f9524f4107f273fed6c2d7
Mitchell <mgerb42@gmail.com>
2025-05-21 20:57:42 -0500

    nvim: set windows terminal to powershell

--- config/nvim/lua/mg/_windows.lua
+++ config/nvim/lua/mg/_windows.lua
@@ -1,11 +1,17 @@
 --- Window specific options
 
 -- Set the shell to Powershell on Windows
--- if vim.fn.has("win32") == 1 then
--- 	if vim.fn.executable("bash.exe") then
--- 		vim.cmd("set shell=bash.exe")
--- 	else
--- 		vim.cmd("set shell=powershell.exe")
--- 	end
--- 	vim.cmd("set shellcmdflag=-NoProfile\\ -Command")
--- end
+if vim.fn.has("win32") == 1 then
+	-- bash had some weird behavior, but it wasn't consisten
+	-- across all windows machines I have used. Just stick
+	-- to powershell.
+	--
+	-- if vim.fn.executable("bash.exe") then
+	-- 	vim.cmd("set shell=bash.exe")
+	-- else
+	-- 	vim.cmd("set shell=powershell.exe")
+	-- end
+
+	vim.cmd("set shell=powershell.exe")
+	vim.cmd("set shellcmdflag=-NoProfile\\ -Command")
+end
