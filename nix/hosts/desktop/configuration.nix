d42e96119d4a52f27c0ced147beaf1b9227d06e3
Mitchell Gerber <mgerb42@gmail.com>
2025-09-14 19:41:04 -0500

    nix: hyprland updates

--- nix/hosts/desktop/configuration.nix
+++ nix/hosts/desktop/configuration.nix
@@ -51,7 +51,7 @@
 
   services.displayManager.sddm.enable = true;
   # Enable the KDE Plasma Desktop Environment.
-  # services.desktopManager.plasma6.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
