88ba8cea9e858e55c88207d8a365eea6504445c4
Mitchell Gerber <mgerb42@gmail.com>
2026-01-11 08:23:36 -0600

    nix: update thinkpad, intel_graphics

--- nix/hosts/thinkpad/modules/intel_graphics.nix
+++ nix/hosts/thinkpad/modules/intel_graphics.nix
@@ -2,6 +2,8 @@
   hardware.graphics = {
     enable = true;
     extraPackages = with pkgs; [
+      intel-vaapi-driver
+      intel-media-driver
       #... # your Open GL, Vulkan and VAAPI drivers
       vpl-gpu-rt # for newer GPUs on NixOS >24.05 or unstable
       # onevpl-intel-gpu  # for newer GPUs on NixOS <= 24.05
