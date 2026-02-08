541f521cded473e2b5a3a2db4fc62c804907369c
Mitchell <mgerb42@gmail.com>
2026-02-08 11:23:13 -0600

    nix: tailscale - update client config - use exit node

--- nix/modules/tailscale-client.nix
+++ nix/modules/tailscale-client.nix
@@ -4,6 +4,8 @@
     useRoutingFeatures = "client";
     extraSetFlags = [
       "--accept-routes"
+      # To disable the exit node: `sudo tailscale set --exit-node=`
+      "--exit-node=snixos"
     ];
   };
 }
