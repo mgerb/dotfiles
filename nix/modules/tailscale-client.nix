50aa9be6ec64fdfe421f8a68a7921dcb4d2929d2
Mitchell <mgerb42@gmail.com>
2026-02-09 12:35:39 -0600

    nix: update desktop

--- nix/modules/tailscale-client.nix
+++ nix/modules/tailscale-client.nix
@@ -1,11 +1,10 @@
+# Configure exit node: `sudo tailscale set --exit-node=snixos`
 {...}: {
   services.tailscale = {
     enable = true;
     useRoutingFeatures = "client";
     extraSetFlags = [
       "--accept-routes"
-      # To disable the exit node: `sudo tailscale set --exit-node=`
-      "--exit-node=snixos"
     ];
   };
 }
