81280841177382972283f1eb4bc493a38f46199c
Mitchell <mgerb42@gmail.com>
2026-03-29 13:48:09 -0500

    nix: desktop - ntsync

--- nix/hosts/desktop/hardware-configuration.nix
+++ nix/hosts/desktop/hardware-configuration.nix
@@ -14,7 +14,7 @@
 
   boot.initrd.availableKernelModules = ["vmd" "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod"];
   boot.initrd.kernelModules = [];
-  boot.kernelModules = ["kvm-intel"];
+  boot.kernelModules = ["kvm-intel" "ntsync"];
   boot.extraModulePackages = [];
 
   fileSystems."/" = {
