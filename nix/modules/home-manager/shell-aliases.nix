a0cb251a2f2f36aafc642d58e9cc319b4a7dde9e
Mitchell <mgerb42@gmail.com>
2026-05-03 09:12:44 -0500

    nix: update docker compose shell alias

--- nix/modules/home-manager/shell-aliases.nix
+++ nix/modules/home-manager/shell-aliases.nix
@@ -1,7 +1,8 @@
 {...}: {
   home.shellAliases = {
     hm = "home-manager";
-    dc = "docker compose";
+    # --progress=plain - This fixes output in the Neovim terminal.
+    dc = "docker compose --progress=plain";
     # docker compose pull, down, update
     mgdu = "dc pull && dc down && dc up -d";
     mgspace = "du -h -d 1 | sort -h";
